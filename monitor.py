d11c4e7b3aa3ef845fee6517b2321350d8c7a905
unknown <ni95@purdue.edu>
2021-06-29 06:59:15 -0500

    Proxy support

--- monitor.py
+++ monitor.py
@@ -3,39 +3,57 @@ import requests
 from bs4 import BeautifulSoup
 from discord_webhook import DiscordWebhook
 from datetime import datetime
-import os
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'
 }
 
+proxyList = []
+count = 0
+
+with open('proxyList.txt') as f:
+    for line in f:
+        line = line.strip()
+        line = line.split(':')
+        proxyHTTP = line[0] + ':' + line[1]
+        proxyList.append(proxyHTTP)
+proxyListLength = len(proxyList)
+
 while True:
-    #try:
-        with open('productList.txt') as f:
-            for line in f:
-                line = line.strip()
-                productURL = line
-                response = requests.get(productURL, headers=headers)
-                soup = BeautifulSoup(response.text, 'html.parser')
+    with open('productList.txt') as f:
+        for line in f:
+            line = line.strip()
+            productURL = line
 
-                productName = soup.find('h1', attrs={'class', 'v-fw-regular'}).get_text()
-                imageURL = soup.find('img', attrs={'class', 'primary-image'})['src']
+            proxies = {
+                "http": "http://" + proxyList[count],
+                "https": "http://" + proxyList[count],
+            }
 
-                rawSKU = soup.find('div', attrs={'class': 'sku product-data'}).get_text().strip()
-                sku = rawSKU.split('SKU:')[1]
+            count += 1
+            if count == proxyListLength:
+                count = 0
 
-                rawPrice = soup.find('div', attrs={'class': 'priceView-hero-price'}).get_text()
-                price = rawPrice.split('Your')[0]
+            response = requests.get(productURL, headers=headers)
+            response.proxies = proxies
+            print(response.proxies)
+            soup = BeautifulSoup(response.text, 'html.parser')
 
-                ATC = 'https://api.bestbuy.com/click/-/' + sku + '/cart'
+            productName = soup.find('h1', attrs={'class', 'v-fw-regular'}).get_text()
+            imageURL = soup.find('img', attrs={'class', 'primary-image'})['src']
 
-                availability = soup.find('button', attrs={'class': 'add-to-cart-button'}).get_text()
+            rawSKU = soup.find('div', attrs={'class': 'sku product-data'}).get_text().strip()
+            sku = rawSKU.split('SKU:')[1]
 
-                if (availability == 'Add to Cart'):
-                    sendAlert(productURL, productName, price, sku, imageURL, ATC)
-                    print(productName + ' RESTOCKED')
-                else:
-                    print(productName + ' OOS')
-    #except ConnectionRefusedError:
-        #os.system("python monitor.py")
-        #print("Restarting...")
+            rawPrice = soup.find('div', attrs={'class': 'priceView-hero-price'}).get_text()
+            price = rawPrice.split('Your')[0]
+
+            ATC = 'https://api.bestbuy.com/click/-/' + sku + '/cart'
+
+            availability = soup.find('button', attrs={'class': 'add-to-cart-button'}).get_text()
+
+            if (availability == 'Add to Cart'):
+                sendAlert(productURL, productName, price, sku, imageURL, ATC)
+                print(productName + ' RESTOCKED')
+            else:
+                print(productName + ' OOS')
--- test.py
+++
@@ -1,8 +0,0 @@
-with open('productList.txt') as f:
-        for line in f:
-            line = line.strip()
-            productURL = line
-
-            print(productURL)
-            
-            
